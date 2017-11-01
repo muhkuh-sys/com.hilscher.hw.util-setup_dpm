088a5c4eacb88dda675c172b7bc10b1e70525df2
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-11-01 11:21:38 +0100

    Check if the code is running on a netX4000 RELAXED.

--- src/netx4000/setup_dpm.c
+++ src/netx4000/setup_dpm.c
@@ -46,6 +46,18 @@ static BOOT_MODE_LED_T get_boot_mode(void)
 
 
 
+typedef struct
+{
+	/* This is the vector table at the start of the ROM. */
+	unsigned long aulVectorTable[8];
+	/* Version and chip type. */
+	unsigned long ulInfo;
+} ROMVECTOR_T;
+
+extern ROMVECTOR_T tRomVector;
+#define ROMVECTOR_INFO_netX4000_RELAXED 0x00108004
+
+
 void __attribute__ ((section (".init_code"))) start(void);
 void start(void)
 {
@@ -56,53 +68,57 @@ void start(void)
 	/* Be pessimistic. */
 	iResult = -1;
 
-	/* Get the boot mode. */
-	tBootMode = get_boot_mode();
-	switch(tBootMode)
+	/* Detect if this is really a relaxed chip. */
+	if( tRomVector.ulInfo==ROMVECTOR_INFO_netX4000_RELAXED )
 	{
-	case BootModeLed_REE_SD:
-		/* Nothing to setup. */
-		iResult = 0;
-		break;
-
-	case BootModeLed_netX_HIF_DPM_serial:
-		/* Setup the serial DPM. */
-		tResult = boot_dpm(DPM_TRANSPORT_TYPE_Serial);
-		if( tResult==BOOTING_Ok )
+		/* Get the boot mode. */
+		tBootMode = get_boot_mode();
+		switch(tBootMode)
 		{
+		case BootModeLed_REE_SD:
+			/* Nothing to setup. */
 			iResult = 0;
-		}
-		break;
+			break;
 
-	case BootModeLed_netX_ETH_LVDS:
-	case BootModeLed_minimal_update:
-		/* Nothing to setup. */
-		iResult = 0;
-		break;
+		case BootModeLed_netX_HIF_DPM_serial:
+			/* Setup the serial DPM. */
+			tResult = boot_dpm(DPM_TRANSPORT_TYPE_Serial);
+			if( tResult==BOOTING_Ok )
+			{
+				iResult = 0;
+			}
+			break;
 
-	case BootModeLed_REE_DPM_PCIE:
-		/* Setup the PCI express DPM. */
-		tResult = boot_pcie();
-		if( tResult==BOOTING_Ok )
-		{
+		case BootModeLed_netX_ETH_LVDS:
+		case BootModeLed_minimal_update:
+			/* Nothing to setup. */
 			iResult = 0;
-		}
-		break;
+			break;
 
-	case BootModeLed_netX_HIF_DPM_parallel:
-		/* Setup the parallel DPM. */
-		tResult = boot_dpm(DPM_TRANSPORT_TYPE_Parallel);
-		if( tResult==BOOTING_Ok )
-		{
+		case BootModeLed_REE_DPM_PCIE:
+			/* Setup the PCI express DPM. */
+			tResult = boot_pcie();
+			if( tResult==BOOTING_Ok )
+			{
+				iResult = 0;
+			}
+			break;
+
+		case BootModeLed_netX_HIF_DPM_parallel:
+			/* Setup the parallel DPM. */
+			tResult = boot_dpm(DPM_TRANSPORT_TYPE_Parallel);
+			if( tResult==BOOTING_Ok )
+			{
+				iResult = 0;
+			}
+			break;
+
+		case BootModeLed_REE_PL353_NAND:
+		case BootModeLed_netX_MEM_ETH:
+			/* Nothing to setup. */
 			iResult = 0;
+			break;
 		}
-		break;
-
-	case BootModeLed_REE_PL353_NAND:
-	case BootModeLed_netX_MEM_ETH:
-		/* Nothing to setup. */
-		iResult = 0;
-		break;
 	}
 
 	if( iResult!=0 )
